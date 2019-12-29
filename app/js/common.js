ac11825a373b728205b662b2369a924b48977219
Artem Saienko <fitzsyke@gmail.com>
2019-12-29 22:54:04 +0200

    home => partners tabs

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
+	// home partners tabs
+	function initPartnersTabs(activate) {
+		const partners = document.querySelectorAll('.partners-item');
+		
+		if (activate) {
+			partners.forEach((partner, index) => {
+				if (index === 0) {
+					partner.classList.add('active');
+				}
+				function addClassToActive() {
+					partners.forEach(el => { el.classList.remove('active'); })
+					partner.classList.add('active');
+				}
+				partner.addEventListener('click', addClassToActive);
+			});
+		} else {
+			partners.forEach(partner => {
+				partner.removeEventListener('click', addClassToActive);
+			});
+		}
+	}
+	initPartnersTabs(true);
+
 	$('.our-team-cart').children('.our-team-cart-info').click(function () {
 		if ($(this).children('.our-team-cart-info-p-hidden').is(":hidden")) {
 			$('.our-team-cart-active').removeClass('our-team-cart-active');

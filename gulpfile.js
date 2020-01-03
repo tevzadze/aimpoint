12ee5fc5693ac9502f78572adade71caf72783b2
Andrii Starusiev <a.starusiev@zajno.com>
2020-01-03 12:21:59 +0200

    gulp scripts allowEmpty

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('scripts', function() {
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/slick/slick.min.js',
 		'app/js/common.js', // Always at the end
-		])
+		], { allowEmpty: true })
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
